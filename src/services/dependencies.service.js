9d3094c8ab17361c0a2c6ae3bd283766c2f9cf14
Josh Comeau <joshwcomeau@gmail.com>
2018-06-26 09:48:56 -0400

    Move dependency disk operations to a service

---
+++ src/services/dependencies.service.js
@@ -0,0 +1,42 @@
+// @flow
+const childProcess = window.require('child_process');
+
+export const installDependency = (
+  projectPath: string,
+  dependencyName: string,
+  version: string
+) => {
+  return new Promise((resolve, reject) => {
+    // TODO: yarn?
+    childProcess.exec(
+      `npm install ${dependencyName}@${version} -SE`,
+      { cwd: projectPath },
+      (err, res) => {
+        err ? reject(err) : resolve(res);
+      }
+    );
+  });
+};
+
+export const uninstallDependency = (
+  projectPath: string,
+  dependencyName: string
+) => {
+  return new Promise((resolve, reject) => {
+    childProcess.exec(
+      `npm uninstall ${dependencyName}`,
+      { cwd: projectPath },
+      (err, res) => {
+        err ? reject(err) : resolve(res);
+      }
+    );
+  });
+};
+
+export const reinstallDependencies = (projectPath: string) => {
+  return new Promise((resolve, reject) => {
+    childProcess.exec('npm install', { cwd: projectPath }, (err, res) => {
+      err ? reject(err) : resolve(res);
+    });
+  });
+};
