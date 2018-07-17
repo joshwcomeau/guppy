6f1bdb0a4bd648dea6485b5329b7e76ee03cd829
nikolaevskiy.m <mik639inc@gmail.com>
2018-07-17 23:35:52 +0400

    Tests for dependencies reducer

--- src/reducers/dependencies.reducer.test.js
+++ src/reducers/dependencies.reducer.test.js
@@ -16,12 +16,10 @@ describe('dependencies reducer', () => {
   it('should return initial state', () => {
     expect(reducer(undefined, {})).toEqual({});
   });
-
   it(`should handle ${LOAD_DEPENDENCY_INFO_FROM_DISK}`, () => {
     const prevState = {
       baz: {},
     };
-
     const action = {
       type: LOAD_DEPENDENCY_INFO_FROM_DISK,
       projectId: 'foo',
