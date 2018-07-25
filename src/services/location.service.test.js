672996b7daed02e44d7c5b8990ca9821019c7bd6
Scott Warren <realscottwarren@gmail.com>
2018-07-25 20:45:11 +0300

    Adding checks for when the path doesn't match

--- src/services/location.service.test.js
+++ src/services/location.service.test.js
@@ -15,11 +15,27 @@ describe('Location Service', () => {
     test('should extract the project ID from the URL', () => {
       expect(extractProjectIdFromUrl(stubLocation)).toEqual('some-project-id');
     });
+
+    it('should not return anything when the URL does not contain a project ID', () => {
+      const locationExample = {
+        pathname: '/some/path',
+      };
+
+      expect(extractSelectedTaskFromUrl(locationExample)).toEqual(null);
+    });
   });
 
   describe('extractProjectTabFromUrl', () => {
     it('should extract the project tab from the url', () => {
       expect(extractProjectTabFromUrl(stubLocation)).toEqual('tasks');
+    });
+
+    it('should not return anything when the URL does not contain a project tab', () => {
+      const locationExample = {
+        pathname: '/some/path',
+      };
+
+      expect(extractSelectedTaskFromUrl(locationExample)).toEqual(null);
     });
   });
 
