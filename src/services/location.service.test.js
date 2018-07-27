1df4630fb0a7d971c01a1dba921e63615a87ae69
Scott Warren <realscottwarren@gmail.com>
2018-07-27 19:24:32 +0300

    Adds Location Service tests (#113)
    * Adds tests for the location service
    
    * Adds checks for when the path doesn't match

---
+++ src/services/location.service.test.js
@@ -0,0 +1,70 @@
+import {
+  buildUrlForProjectId,
+  buildUrlForProjectTask,
+  extractSelectedTaskFromUrl,
+  extractProjectIdFromUrl,
+  extractProjectTabFromUrl,
+} from './location.service';
+
+describe('Location Service', () => {
+  const stubLocation = {
+    pathname: '/project/some-project-id/tasks/sometaskname',
+  };
+
+  describe('extractProjectIdFromUrl', () => {
+    test('should extract the project ID from the URL', () => {
+      expect(extractProjectIdFromUrl(stubLocation)).toEqual('some-project-id');
+    });
+
+    it('should not return anything when the URL does not contain a project ID', () => {
+      const locationExample = {
+        pathname: '/some/path',
+      };
+
+      expect(extractProjectIdFromUrl(locationExample)).toEqual(null);
+    });
+  });
+
+  describe('extractProjectTabFromUrl', () => {
+    it('should extract the project tab from the url', () => {
+      expect(extractProjectTabFromUrl(stubLocation)).toEqual('tasks');
+    });
+
+    it('should not return anything when the URL does not contain a project tab', () => {
+      const locationExample = {
+        pathname: '/some/path',
+      };
+
+      expect(extractProjectTabFromUrl(locationExample)).toEqual(null);
+    });
+  });
+
+  describe('extractSelectedTaskFromUrl', () => {
+    it('should extract the selected task from the url', () => {
+      expect(extractSelectedTaskFromUrl(stubLocation)).toEqual('sometaskname');
+    });
+
+    it('should not return anything when the URL does not contain a task', () => {
+      const projectLocation = {
+        pathname: '/project/some-project-id',
+      };
+
+      expect(extractSelectedTaskFromUrl(projectLocation)).toEqual(null);
+    });
+  });
+
+  describe('buildUrlForProjectId', () => {
+    test('should return the correct project url based on the project ID', () => {
+      expect(buildUrlForProjectId('some-project-id')).toEqual(
+        '/project/some-project-id'
+      );
+    });
+  });
+  describe('buildUrlForProjectTask', () => {
+    test('should return the correct task url based on the project ID and task name', () => {
+      expect(
+        buildUrlForProjectTask('some-project-id', 'some-task-name')
+      ).toEqual('/project/some-project-id/tasks/some-task-name');
+    });
+  });
+});
