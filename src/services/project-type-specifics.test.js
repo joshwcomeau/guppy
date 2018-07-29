2bb3ee90868824ba4b0b9ffea51187550cc4ca91
Scott Warren <realscottwarren@gmail.com>
2018-07-29 11:56:38 +0300

    Running Prettier over Project type specifics

--- src/services/project-type-specifics.test.js
+++ src/services/project-type-specifics.test.js
@@ -1,18 +1,25 @@
-import { getDocumentationLink } from './project-type-specifics'
+import { getDocumentationLink } from './project-type-specifics';
 
 import type { ProjectType } from '../types';
 
 describe('getDocumentationLink', () => {
-  it("should get the documentation link for React", () => {
-    expect(getDocumentationLink('create-react-app')).toEqual('https://github.com/facebook/create-react-app#user-guide')
-  })
+  it('should get the documentation link for React', () => {
+    expect(getDocumentationLink('create-react-app')).toEqual(
+      'https://github.com/facebook/create-react-app#user-guide'
+    );
+  });
 
   it('should get the documentation link for Gatsby', () => {
-    expect(getDocumentationLink('gatsby')).toEqual('https://www.gatsbyjs.org/docs/')
-  })
+    expect(getDocumentationLink('gatsby')).toEqual(
+      'https://www.gatsbyjs.org/docs/'
+    );
+  });
 
   it('should throw an exception if passed a project type that is not defined', () => {
-    const unknownProjectType = 'some-unknown-project-type'
-    expect(() => getDocumentationLink(unknownProjectType)).toThrowError(`Unrecognized project type: ${unknownProjectType}`)
-  })
-})
+    const unknownProjectType = 'some-unknown-project-type';
+
+    expect(() => getDocumentationLink(unknownProjectType)).toThrowError(
+      `Unrecognized project type: ${unknownProjectType}`
+    );
+  });
+});
