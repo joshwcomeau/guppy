8671d6cba2ea6feab623365119a1607a85de922e
AWolf81 <awolf2904@gmail.com>
2018-08-04 10:41:53 +0200

    simplified spawnProcess

--- src/services/dependencies.service.js
+++ src/services/dependencies.service.js
@@ -1,73 +1,35 @@
 // @flow
 import { PACKAGE_MANAGER_CMD } from './platform.services';
+import { ChildProcess } from 'child_process';
 const childProcess = window.require('child_process');
 
-/* TODO: Improve flow type for executor - no type annotation found
-         from facebook flow source https://github.com/facebook/flow/blob/v0.76.0/lib/core.js#L584:
-          constructor(callback: (
-                resolve: (result: Promise<R> | R) => void,
-                reject: (error: any) => void
-              ) => mixed): void;
-*/
-
-const spawnProcess = (
-  cmd: string,
-  cmdArgs: Array<string>,
-  projectPath: string,
-  [resolve, reject]: Array<any>
-) => {
-  const child = childProcess.spawn(cmd, cmdArgs, {
-    cwd: projectPath,
+const spawnProcess = (cmd: string, cmdArgs: string[], projectPath: string) =>
+  new Promise((resolve, reject) => {
+    const child = childProcess.spawn(cmd, cmdArgs, {
+      cwd: projectPath,
+    });
+    child.on(
+      'exit',
+      code => (code ? reject(child.stderr) : resolve(child.stdout))
+    );
+    // logger(child) // service will be used here later
   });
-
-  child.on('exit', code => {
-    if (code) {
-      reject(child.stderr);
-    } else {
-      resolve(child.stdout);
-    }
-  });
-
-  return child;
-};
 
 export const installDependency = (
   projectPath: string,
   dependencyName: string,
   version: string
-) => {
-  return new Promise(function(resolve, reject) {
-    // no arrow function so we can use function arguments to pass
-    const child = spawnProcess(
-      PACKAGE_MANAGER_CMD,
-      ['add', `${dependencyName}@${version}`, '-SE'],
-      projectPath,
-      arguments
-    );
-  });
-};
+) =>
+  spawnProcess(
+    PACKAGE_MANAGER_CMD,
+    ['add', `${dependencyName}@${version}`, '-SE'],
+    projectPath
+  );
 
 export const uninstallDependency = (
   projectPath: string,
   dependencyName: string
-) => {
-  return new Promise(function(resolve, reject) {
-    const child = spawnProcess(
-      PACKAGE_MANAGER_CMD,
-      ['remove', dependencyName],
-      projectPath,
-      arguments
-    );
-  });
-};
+) => spawnProcess(PACKAGE_MANAGER_CMD, ['remove', dependencyName], projectPath);
 
-export const reinstallDependencies = (projectPath: string) => {
-  return new Promise(function(resolve, reject) {
-    const child = spawnProcess(
-      PACKAGE_MANAGER_CMD,
-      ['install'],
-      projectPath,
-      arguments
-    );
-  });
-};
+export const reinstallDependencies = (projectPath: string) =>
+  spawnProcess(PACKAGE_MANAGER_CMD, ['install'], projectPath);
