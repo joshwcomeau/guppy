5804ea4d51ba5ca63c0fac66ff82e526a7bfa676
AWolf81 <awolf2904@gmail.com>
2018-08-12 23:38:06 +0200

    WIP: Adding props addon - not working yet.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
-import { configure } from '@storybook/react';
+import { configure, setAddon } from '@storybook/react';
 import '../src/global-styles';
+
+import addWithDoc from 'storybook-addon-props';
+
+setAddon(addWithDoc);
 
 const components = require.context('../src/components', true, /.stories.js$/);
 
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Button from './Button';
@@ -6,13 +7,33 @@ import Button from './Button';
 const SIZES = ['small', 'medium', 'large'];
 
 storiesOf('Button', module)
-  .add('Default', () => <Button onClick={action('clicked')}>Button</Button>)
-  .add('with size', () => (
-    <div>
-      {SIZES.map((size, i) => (
-        <Button onClick={action('clicked')} size={size} key={i}>
-          Button {size}
-        </Button>
-      ))}
-    </div>
+  .addWithDoc('with label', Button,
+    'It should render a button',
+    () => <Button onClick={action('clicked')}>Hello Button</Button>
   ));
+  // .add('Default', () => <Button onClick={action('clicked')}>Button</Button>)
+
+  // .add('with size', () => (
+  //   <Wrapper>
+  //     {SIZES.map((size, i) => (
+  //       <Button onClick={action('clicked')} size={size} key={i}>
+  //         Button {size}
+  //       </Button>
+  //     ))}
+  //   </Wrapper>
+  // ))
+  // .add('with type', () => (
+  //   <Wrapper>
+  //     <Button onClick={action('clicked')}>Button normal</Button>
+  //     <Button onClick={action('clicked')} type="fill">
+  //       Button fill
+  //     </Button>
+  //     <Button onClick={action('clicked')} type="stroke">
+  //       Button stroke
+  //     </Button>
+  //   </Wrapper>
+  // ));
+
+const Wrapper = styled.div`
+  padding: 1em;
+`;
