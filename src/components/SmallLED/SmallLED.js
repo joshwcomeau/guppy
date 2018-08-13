1bd8622bb5f1f16e02a3b53a6029d6eb268ba7b4
mathieudutour <mathieu@dutour.me>
2018-08-13 13:41:48 -0600

    fix lint errors

--- src/components/SmallLED/SmallLED.js
+++ src/components/SmallLED/SmallLED.js
@@ -1,5 +1,4 @@
 // @flow
-import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
--- src/services/create-project-service.test.js
+++ src/services/create-project-service.test.js
@@ -4,6 +4,7 @@ jest.mock('../reducers/paths.reducer.js', () => ({
   getDefaultParentPath: jest.fn(),
 }));
 
+// eslint-disable-next-line import/first
 import {
   possibleProjectColors,
   getColorForProject,
