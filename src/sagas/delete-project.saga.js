2e7a6ef709b9f0ca2eca791a49d23c3bc46de621
Aaron Ross <superhawk610@gmail.com>
2018-09-01 09:25:26 -0400

    add cancelId (#188)

--- src/sagas/delete-project.saga.js
+++ src/sagas/delete-project.saga.js
@@ -58,6 +58,7 @@ export function* deleteProject({ project }: Action): Saga<void> {
     type: 'warning',
     buttons: ['Delete from Disk', 'Cancel'],
     defaultId: 0,
+    cancelId: 1,
     title: `Delete ${project.name}`,
     message: `Are you sure you want to delete ${project.name}?`,
     detail: 'WARNING! Deleting from disk will send the project to trash!',
