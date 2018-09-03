cb6d04fc09e1cf353bb9a0acbe2821cfa2c90e92
Josh Comeau <joshwcomeau@gmail.com>
2018-09-03 17:21:05 -0400

    get all components assembed in WhimsicalInstaller

--- src/components/WhimsicalInstaller/WhimsicalInstaller.js
+++ src/components/WhimsicalInstaller/WhimsicalInstaller.js
@@ -1,9 +1,63 @@
+// @flow
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
-class WhimsicalInstaller extends Component {
+import File from './File';
+import Folder from './Folder';
+import Earth from '../Earth';
+
+type Props = {
+  width: number,
+};
+
+class WhimsicalInstaller extends Component<Props> {
   render() {
-    return <div />;
+    const { width } = this.props;
+
+    // Our height will be 1/3rd of our width.
+    // This is so we end up with 3 squares:
+    //  _____________________
+    // |      |      |      |
+    // |      |      |      |
+    // |______|______|______|
+    //
+    const height = width / 3;
+
+    return (
+      <Wrapper>
+        <PlanetContainer size={height}>
+          <Earth size={height / 2} />
+        </PlanetContainer>
+        <File size={height / 4} x={500} y={60} />
+        <FolderContainer size={height}>
+          <Folder size={height / 3} />
+        </FolderContainer>
+      </Wrapper>
+    );
   }
 }
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PlanetContainer = styled.div`
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FolderContainer = styled.div`
+  position: relative;
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default WhimsicalInstaller;
--- src/components/WhimsicalInstaller/WhimsicalInstaller.stories.js
+++ src/components/WhimsicalInstaller/WhimsicalInstaller.stories.js
@@ -9,29 +9,32 @@ import { COLORS } from '../../constants';
 import Showcase from '../../../.storybook/components/Showcase';
 import File from './File';
 import Folder from './Folder';
+import WhimsicalInstaller from './WhimsicalInstaller';
 
-storiesOf('WhimsicalInstaller', module).add(
-  'components',
-  withInfo()(() => (
+storiesOf('WhimsicalInstaller', module)
+  .add('components', () => (
     <Fragment>
       <Showcase label="File">
-        <Wrapper>
+        <Wrapper width={100} height={100}>
           <File x={10} y={10} />
         </Wrapper>
       </Showcase>
       <Showcase label="Folder">
-        <Wrapper>
+        <Wrapper width={100} height={100}>
           <Folder size={60} />
         </Wrapper>
       </Showcase>
     </Fragment>
   ))
-);
+  .add('All together', () => (
+    <Wrapper width={600} height={200}>
+      <WhimsicalInstaller width={600} />
+    </Wrapper>
+  ));
 
 const Wrapper = styled.div`
   position: relative;
-  padding: 10px;
-  width: 100px;
-  height: 100px;
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
   background: ${COLORS.blue[700]};
 `;
