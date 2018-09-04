d8fcc3c137bd0d03954259e741a270e38174f721
AWolf81 <awolf2904@gmail.com>
2018-09-04 23:59:32 +0200

    remove unused import & comment

--- src/sagas/save-project-settings.saga.test.js
+++ src/sagas/save-project-settings.saga.test.js
@@ -1,15 +1,17 @@
 import electron from 'electron'; // Mocked
-import { call, put, select, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
 
 import rootSaga, {
   handleSaveSettings,
+  handleFinishSettings,
   renameFolder,
 } from './save-project-settings.saga';
 
 import {
   SAVE_PROJECT_SETTINGS_START,
-  // saveProjectSettingsStart,
+  SAVE_PROJECT_SETTINGS_FINISH,
   saveProjectSettingsFinish,
+  hideModal,
 } from '../actions';
 import {
   loadPackageJson,
@@ -18,19 +20,19 @@ import {
 
 jest.mock('path', () => ({
   resolve: jest.fn(),
-  join: jest.fn(),
+  join: () => 'path/to/new-project',
 }));
-
-// jest.mock('fs', () => ({
-//   renameSync: jest.fn(),
-// }));
 
 describe('save-project-settings saga', () => {
   describe('root import-project saga', () => {
     it('should watching for start actions', () => {
       const saga = rootSaga();
+
       expect(saga.next().value).toEqual(
         takeEvery(SAVE_PROJECT_SETTINGS_START, handleSaveSettings)
+      );
+      expect(saga.next().value).toEqual(
+        takeEvery(SAVE_PROJECT_SETTINGS_FINISH, handleFinishSettings)
       );
     });
   });
@@ -39,6 +41,7 @@ describe('save-project-settings saga', () => {
     it('should save settings', () => {
       const { dialog } = electron.remote;
       const json = { name: 'project', guppy: { id: 'project', icon: null } };
+
       const action = {
         name: 'new-project',
         icon: 'icon',
@@ -51,27 +54,48 @@ describe('save-project-settings saga', () => {
         call(loadPackageJson, 'path/to/project')
       );
 
-      // renameFolder called
-      // expect(saga.next().value).toEqual(
-      //   call(renameFolder, 'path/to/project', 'path/to/new-project')
-      // );
-
       // Show confirmation for imported projects if name changed
-      // console.log('next val', saga.next(json).value);
-      // expect(saga.next().value).toEqual(call([dialog, dialog.showMessageBox]));
+      expect(saga.next(json).value).toEqual(
+        call([dialog, dialog.showMessageBox], {
+          type: 'warning',
+          buttons: ['Yeah', 'Nope'],
+          defaultId: 1,
+          cancelId: 1,
+          title: 'Are you sure?',
+          message: 'Do you also want to rename the project folder?',
+        })
+      );
+
+      // Confirm & rename folder
+      expect(saga.next(0).value).toEqual(
+        call(renameFolder, 'path/to/project', 'path/to/new-project')
+      );
+
       // Write package.json
-      // expect(saga.next(json).value).toEqual(call(writePackageJson));
+      const jsonWithGuppy = {
+        name: 'new-project',
+        guppy: {
+          name: 'new-project',
+          id: 'new-project',
+          icon: 'icon',
+        },
+      };
 
-      // expect(saga.next(json).value).toEqual(
-      //   put(
-      //     saveProjectSettingsFinish({
-      //       guppy: { icon: 'icon', id: 'new-project', name: 'new-project' },
-      //       name: 'new-project',
-      //     })
-      //   )
-      // );
+      expect(saga.next().value).toEqual(
+        call(writePackageJson, 'path/to/new-project', jsonWithGuppy)
+      );
 
-      // Should update state & close modal
+      expect(saga.next().value).toEqual(
+        put(
+          saveProjectSettingsFinish(undefined, 'project', 'path/to/new-project') // why is project undefined?
+        )
+      );
+    });
+
+    it('should hide modal on finish', () => {
+      const saga = handleFinishSettings();
+
+      expect(saga.next().value).toEqual(put(hideModal()));
     });
   });
 });
