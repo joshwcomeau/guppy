ab3ddecea366bdfc03d31116eff1532ad735e127
Josh Comeau <joshwcomeau@gmail.com>
2018-09-14 09:00:55 -0400

    Add test factory

--- src/services/test-factories.service.js
+++ src/services/test-factories.service.js
@@ -4,43 +4,49 @@
  * `types.js`, for use in writing automated tests.
  */
 import path from 'path';
+import uuid from 'uuid/v1';
 
 const sept14thTimestamp = 1536927197731;
 
-export const createProject = (overrides: any) => ({
-  id: '6f36787f-3a7a-4b3e-a822-88b0b55db6cd',
-  name: 'Hello World',
-  type: 'create-react-app',
-  icon: 'path/to/icon',
-  color: '#FF0000',
-  createdAt: sept14thTimestamp,
-  dependencies: [
-    {
-      name: 'react',
-      description: '',
-      version: '16.5',
-      homepage: '',
-      license: 'MIT',
-      repository: { type: 'git', url: '/' },
-      status: 'idle',
-      location: 'dependencies',
-    },
-  ],
-  tasks: [
-    {
-      projectId: '6f36787f-3a7a-4b3e-a822-88b0b55db6cd',
-      name: 'start',
-      description: '',
-      type: 'sustained',
-      status: 'idle',
-      command: 'react-scripts start',
-      timeSinceStatusChange: null,
-      logs: [],
-    },
-  ],
-  path: path.join('work', 'hello-world'),
+export const createTask = (overrides: any = {}) => ({
+  projectId: '6f36787f-3a7a-4b3e-a822-88b0b55db6cd',
+  name: 'start',
+  description: '',
+  type: 'sustained',
+  status: 'idle',
+  command: 'react-scripts start',
+  timeSinceStatusChange: null,
+  logs: [],
   ...overrides,
 });
+
+export const createDependency = (overrides: any = {}) => ({
+  name: 'react',
+  description: '',
+  version: '16.5',
+  homepage: '',
+  license: 'MIT',
+  repository: { type: 'git', url: '/' },
+  status: 'idle',
+  location: 'dependencies',
+});
+
+export const createProject = (overrides: any) => {
+  const projectId = overrides ? overrides.projectId : uuid();
+
+  return {
+    id: projectId,
+    name: 'Hello World',
+    type: 'create-react-app',
+    icon: 'path/to/icon',
+    color: '#FF0000',
+    createdAt: sept14thTimestamp,
+    dependencies: [createDependency()],
+    tasks: [createTask({ projectId })],
+    path: path.join('work', 'hello-world'),
+    ...overrides,
+  };
+};
 
 export const createProjectInternal = (overrides: any) => ({
   name: 'hello-world',
