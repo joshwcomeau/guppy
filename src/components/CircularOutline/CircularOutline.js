e1fe6d0d67bfc6c36d47fc76789f539b4e38879e
Josh Comeau <joshwcomeau@gmail.com>
2018-09-25 08:07:10 -0400

    Switch to fixed size + React Spring for CircularOutline

--- src/components/CircularOutline/CircularOutline.js
+++ src/components/CircularOutline/CircularOutline.js
@@ -1,132 +1,50 @@
 // @flow
 /**
- * Draws a totally-rounded rectangle inside the parent node.
- *
- * NOTE: This turned out to be a surprisingly hard problem! I had to use an
- * SVG since I needed a gradient border, and because the SVG is computed
- * dynamically, a few re-renders need to take place.
- *
- * Here's the flow:
- *   - Initial render:  Capture a reference to the SVG, so we can work out the
- *                      width and height of the parent container
- *
- *   - Path length:     Now that we have the length/height, we can add our SVG
- *                      <rect> that fills the space. After that update is
- *                      flushed to the DOM, we can figure out the length of the
- *                      path with `getTotalLength`.
- *                      TODO: I can probably use math to work out that path
- *                      so that I can skip this update cycle?
- *
- *   - Enabling trans:  We don't want the 'self-drawing' transition to happen
- *                      on mount. Because of this, transition starts as
- *                      disabled.
- *                      Once we make it to this stage, though, we can set
- *                      `finishedAllMountingSteps` to indicate that all this
- *                      stuff is done, so that any future changes to `isShown`
- *                      can be animated
- *
- * There's surely a lot of room for improvement with this flow.
+ * Draws a totally-rounded circle inside the parent node.
  */
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Motion, spring } from 'react-motion';
+import { Spring } from 'react-spring';
 
 import { COLORS } from '../../constants';
 
 type Props = {
+  size: number,
   color1: string,
   color2: string,
   strokeWidth: number,
   isShown: boolean,
-  animateChanges: boolean,
 };
 
-type State = {
-  width: ?number,
-  height: ?number,
-  pathLength: number,
-  finishedAllMountingSteps: boolean,
+const springSettings = {
+  tension: 150,
+  friction: 22,
+  restSpeedThreshold: 3,
+  restDisplacementThreshold: 10,
 };
 
-const springSettings = { stiffness: 150, damping: 22, precision: 5 };
-
-class RoundedOutline extends Component<Props, State> {
-  state = {
-    width: null,
-    height: null,
-    pathLength: 0,
-    finishedAllMountingSteps: false,
-  };
-
-  wrapperNode: HTMLElement;
-  shapeNode: any; // "SVGPathElement" (which cannot be resolved), we need "getTotalLength()"
-
+class RoundedOutline extends Component<Props> {
   static defaultProps = {
     color1: COLORS.purple[500],
     color2: COLORS.violet[500],
     strokeWidth: 2,
-    animateChanges: true,
   };
 
-  componentDidMount() {
-    // On first mount, we need to figure out how big the available area is.
-    // This will affect how large the outline is.
-    const { width, height } = this.wrapperNode.getBoundingClientRect();
-
-    this.setState({ width, height });
-  }
-
-  componentDidUpdate(_: Props, prevState: State) {
-    if (this.state.width !== null && this.state.height !== null) {
-      if (prevState.width == null && prevState.height == null) {
-        const pathLength = this.shapeNode.getTotalLength();
-
-        this.setState({ pathLength });
-      } else {
-        const { width, height } = this.wrapperNode.getBoundingClientRect();
-
-        if (this.state.width !== width || this.state.height !== height) {
-          this.setState({ width, height });
-        }
-      }
-    }
-
-    if (this.state.pathLength !== null) {
-      const newPathLength = this.shapeNode.getTotalLength();
-
-      if (this.state.pathLength !== newPathLength) {
-        this.setState({ pathLength: newPathLength });
-      }
-    }
-
-    if (prevState.pathLength === 0 && this.state.pathLength !== 0) {
-      this.setState({ finishedAllMountingSteps: true });
-    }
-  }
-
   render() {
-    const { color1, color2, strokeWidth, isShown, animateChanges } = this.props;
-    const { width, height, pathLength, finishedAllMountingSteps } = this.state;
+    const { size, color1, color2, strokeWidth, isShown } = this.props;
 
     const svgId = `${color1.replace('#', '')}-${color2.replace('#', '')}`;
 
-    const dashOffset = isShown ? 0 : pathLength;
+    // Yay middle-school maths
+    const radius = size / 2;
+    const circumference = 2 * Math.PI * radius;
+
+    const dashOffset = isShown ? circumference : 0;
 
     return (
-      <Motion
-        style={{
-          interpolatedDashOffset:
-            animateChanges && finishedAllMountingSteps
-              ? spring(dashOffset, springSettings)
-              : dashOffset,
-        }}
-      >
-        {({ interpolatedDashOffset }) => (
-          <Svg
-            innerRef={node => (this.wrapperNode = node)}
-            width="100%"
-            height="100%"
-          >
+      <Spring config={springSettings} to={{ dashOffset }}>
+        {interpolated => (
+          <Svg width={size} height={size}>
             <defs>
               <linearGradient id={svgId} x1="0%" y1="100%" x2="100%" y2="0%">
                 <stop
@@ -145,29 +63,27 @@ class RoundedOutline extends Component<Props, State> {
                 />
               </linearGradient>
             </defs>
-            {typeof width === 'number' &&
-              typeof height === 'number' && (
-                <Rect
-                  innerRef={node => (this.shapeNode = node)}
-                  x={0}
-                  y={0}
-                  width={width}
-                  height={height}
-                  rx={height / 2}
-                  ry={height / 2}
-                  fill="none"
-                  stroke={`url(#${svgId})`}
-                  strokeWidth={strokeWidth}
-                  strokeLinecap="round"
-                  style={{
-                    strokeDasharray: `${pathLength}, ${pathLength + 1}`,
-                    strokeDashoffset: interpolatedDashOffset,
-                  }}
-                />
-              )}
+
+            <ellipse
+              cx={radius}
+              cy={radius}
+              rx={radius}
+              ry={radius}
+              fill="none"
+              stroke={`url(#${svgId})`}
+              strokeWidth={strokeWidth}
+              strokeLinecap="round"
+              style={{
+                strokeDasharray: `
+                  ${circumference},
+                  ${circumference + 1}
+                `,
+                strokeDashoffset: interpolated.dashOffset,
+              }}
+            />
           </Svg>
         )}
-      </Motion>
+      </Spring>
     );
   }
 }
@@ -177,8 +93,7 @@ const Svg = styled.svg`
   position: absolute;
   top: 0;
   left: 0;
+  transform: rotate(-90deg);
 `;
-
-const Rect = styled.rect``;
 
 export default RoundedOutline;
--- src/components/CircularOutline/CircularOutline.stories.js
+++ src/components/CircularOutline/CircularOutline.stories.js
@@ -2,96 +2,40 @@
 import React, { Fragment, Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { Toggle as ToggleState } from 'react-powerplug';
 
 import Showcase from '../../../.storybook/components/Showcase';
 import CircularOutline from './CircularOutline';
+import FillButton from '../Button/FillButton';
 import styled from 'styled-components';
 
-type Props = { children: (data: any) => React$Node };
-type State = { copyIndex: number };
-
-const BUTTON_COPY = [
-  'hello',
-  'hello world',
-  '!',
-  'Wow this is so long why is it so long ahhhhhhhhhhhh',
-];
-class CopyManager extends Component<Props, State> {
-  state = {
-    copyIndex: 0,
-  };
-
-  cycleCopy = () => {
-    // This line means we always get a value between 0-3:
-    const nextCopyIndex = (this.state.copyIndex + 1) % BUTTON_COPY.length;
-
-    this.setState({ copyIndex: nextCopyIndex });
-  };
-
-  render() {
-    const { children } = this.props;
-    const { copyIndex } = this.state;
-
-    const copy = BUTTON_COPY[copyIndex];
-
-    return (
-      <Fragment>
-        <button onClick={this.cycleCopy}>Change Size</button>
-        <br />
-        <br />
-        <ButtonElem>
-          <OutlineWrapper>{children(copy)}</OutlineWrapper>
-          {copy}
-        </ButtonElem>
-      </Fragment>
-    );
-  }
-}
-
 storiesOf('CircularOutline', module).add(
-  'dynamicSizing',
+  'default',
   withInfo()(() => (
-    <Fragment>
-      <Showcase label="Dynamic Sizing">
-        <CopyManager>
-          {copy => (
-            <CircularOutline
-              color1={'red'}
-              color2={'blue'}
-              strokeWidth={2}
-              isShown={true}
-              animateChanges={true}
-            >
-              {copy}
-            </CircularOutline>
-          )}
-        </CopyManager>
-      </Showcase>
-    </Fragment>
+    <Showcase label="Toggleable">
+      <ToggleState>
+        {({ on, toggle }) => (
+          <Fragment>
+            <Wrapper>
+              <CircularOutline size={40} strokeWidth={2} isShown={on} />
+            </Wrapper>
+
+            <br />
+            <br />
+            <FillButton colors={['#333']} onClick={toggle}>
+              Toggle
+            </FillButton>
+          </Fragment>
+        )}
+      </ToggleState>
+    </Showcase>
   ))
 );
 
-const ButtonElem = styled.button`
+const Wrapper = styled.div`
   position: relative;
-  width: ${props => props.size}px;
-  height: ${props => props.size}px;
-  border: none;
-  background: none;
-  outline: none;
-  padding: 0;
-  cursor: pointer;
-
-  &:active rect {
-    stroke-width: 4;
-  }
-`;
-
-const OutlineWrapper = styled.div`
-  position: absolute;
-  z-index: 3;
-  top: -3px;
-  left: -3px;
-  right: -3px;
-  bottom: -3px;
-  pointer-events: none;
+  width: 40px;
+  height: 40px;
+  background: #eee;
+  border-radius: 50%;
 `;
