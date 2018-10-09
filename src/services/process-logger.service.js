dd5e77e55902215b11d1333ee7dbe63be3f60bb1
AWolf81 <awolf2904@gmail.com>
2018-10-10 00:58:49 +0200

    removed winston

--- src/services/process-logger.service.js
+++ src/services/process-logger.service.js
@@ -1,25 +1,17 @@
-const { LOGGING } = require('../config/app');
-const winston = require('winston');
-const { combine } = winston.format;
+import { LOGGING } from '../config/app';
 
-export const winstonLogger = winston.createLogger({
-  format: combine(winston.format.splat(), winston.format.simple()),
-  transports: [new winston.transports.Console()],
-  silent: !LOGGING || process.env.NODE_ENV === 'production', // logging off or production
-});
-
-export const processLogger = (child, label, level = 'info') => {
-  if (!child.stdout) {
-    return; // needed for tests
+export const processLogger = (child, label) => {
+  if (!LOGGING || process.env.NODE_ENV === 'production') {
+    return; // no logging
   }
 
   // Todo: Handle color codes in logging to console (if supported). There are many control characters in the console output.
   child.stdout.on('data', data => {
     // data is an uint8 array --> decode to string
-    winstonLogger.log(level, '[%s]: %s', label, data.toString());
+    console.log('[%s]: %s', label, data.toString());
   });
 
   child.stderr.on('data', data => {
-    winstonLogger.log('error', '[%s]: %s', label, data.toString());
+    console.error('[%s]: %s', label, data.toString());
   });
 };
