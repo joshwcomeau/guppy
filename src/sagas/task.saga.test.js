5906b95482ba7b6f59fba56104ae4f2053536ef9
Melanie Seltzer <melleh11@gmail.com>
2018-11-13 10:41:04 -0800

    Remove node_modules before install after eject

--- src/sagas/task.saga.test.js
+++ src/sagas/task.saga.test.js
@@ -20,6 +20,7 @@ import {
   completeTask,
   loadDependencyInfoFromDiskStart,
 } from '../actions';
+import { waitForAsyncRimraf } from './delete-project.saga';
 import killProcessId from '../services/kill-process-id.service';
 import { getProjectById } from '../reducers/projects.reducer';
 import { getPathForProjectId } from '../reducers/paths.reducer';
@@ -282,6 +283,16 @@ describe('task saga', () => {
 
         // `take` a log message
         saga.next();
+
+        // delete node_modules folder
+        expect(
+          saga.next({
+            channel: 'exit',
+            timestamp,
+            wasSuccessful: true,
+            uncleanRepo: false,
+          }).value
+        ).toEqual(call(waitForAsyncRimraf, projectPath));
 
         // ejecting requires that dependencies are reinstalled
         const installProcessDescription = call(
