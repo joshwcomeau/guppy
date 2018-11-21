032cab44240c052ebd15b5f687bc0d438934c061
AWolf81 <awolf2904@gmail.com>
2018-11-22 00:18:58 +0100

    added test for config variable substitution

---
+++ src/services/config-variables.service.test.js
@@ -0,0 +1,34 @@
+// @flow
+import { substituteConfigVariables } from './config-variables.service';
+
+describe('substitute config variables', () => {
+  it('should replace $values with real values', () => {
+    const configuration = {
+      env: {
+        cwd: '$projectPath',
+        PORT: '$port',
+      },
+      create: ['npx', '$projectPath', '$projectStarter'],
+    };
+
+    expect(
+      substituteConfigVariables(configuration, {
+        $port: 3000,
+        $projectPath: 'some/path/to/project',
+        $projectStarter: 'https://github.com/gatsbyjs/gatsby-starter-default',
+      })
+    ).toMatchInlineSnapshot(`
+Object {
+  "create": Array [
+    "npx",
+    "some/path/to/project",
+    "https://github.com/gatsbyjs/gatsby-starter-default",
+  ],
+  "env": Object {
+    "PORT": 3000,
+    "cwd": "some/path/to/project",
+  },
+}
+`);
+  });
+});
