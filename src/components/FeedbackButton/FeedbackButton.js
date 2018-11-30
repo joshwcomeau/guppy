9d294ffef8245dbf3ec9d7592383083d3b065523
AWolf81 <awolf2904@gmail.com>
2018-11-30 09:07:31 +0100

    WIP: Add slider animation. Slider not working yet.

--- src/components/FeedbackButton/FeedbackButton.js
+++ src/components/FeedbackButton/FeedbackButton.js
@@ -5,8 +5,6 @@ import IconBase from 'react-icons-kit';
 import { messageSquare } from 'react-icons-kit/feather/messageSquare';
 import { Spring, animated, interpolate } from 'react-spring';
 
-import DetectActive from '../DetectActive';
-
 import { openWindow } from '../../services/shell.service';
 import { IN_APP_FEEDBACK_URL } from '../../config/app';
 import { COLORS } from '../../constants';
@@ -17,57 +15,115 @@ type Props = {
   hoverColor: string,
 };
 
-class FeedbackButton extends PureComponent<Props> {
+type State = {
+  hovered: boolean,
+};
+
+class FeedbackButton extends PureComponent<Props, State> {
   static defaultProps = {
-    size: 36,
+    size: 28,
     color: COLORS.gray[400],
     hoverColor: COLORS.purple[500],
+  };
+
+  state = {
+    hovered: false,
+  };
+
+  handleMouseOver = () => {
+    this.setState({
+      hovered: true,
+    });
+  };
+  handleMouseLeave = () => {
+    this.setState({
+      hovered: false,
+    });
   };
 
   render() {
     const { color, hoverColor, size } = this.props;
     return (
-      <DetectActiveWrapper>
-        {(_, isHovered) => (
-          <Spring
-            native
-            to={{
-              scale: isHovered ? 1.15 : 1,
-            }}
+      <Spring
+        config={{
+          tension: 200,
+          friction: 25,
+          precision: 0.01,
+        }}
+        to={{ hovered: this.state.hovered ? 1.0 : 0 }}
+      >
+        {({ hovered }) => (
+          <Wrapper
+            onMouseLeave={this.handleMouseLeave}
+            onMouseOver={this.handleMouseOver}
+            onClick={() => openWindow(IN_APP_FEEDBACK_URL)}
           >
-            {({ scale }) => (
-              <IconWrapper scale={scale} color={isHovered ? hoverColor : color}>
-                <IconBase
-                  size={size}
-                  icon={messageSquare}
-                  onClick={() => openWindow(IN_APP_FEEDBACK_URL)}
-                />
-              </IconWrapper>
-            )}
-          </Spring>
+            <Text hovered={hovered}>Feedback</Text>
+            <Icon
+              size={size}
+              icon={messageSquare}
+              hovered={hovered}
+              color={color}
+              hovercolor={hoverColor}
+            />
+          </Wrapper>
         )}
-      </DetectActiveWrapper>
+      </Spring>
     );
   }
 }
 
-const IconWrapper = animated(
-  styled.div.attrs({
-    style: ({ scale }) => ({
-      transform: `scale(${scale}, ${scale})`,
-    }),
-  })`
-    color: ${props => props.color};
-  `
-);
-
-const DetectActiveWrapper = styled(DetectActive)`
+const Wrapper = styled.div`
   position: fixed;
-  width: 40px;
-  height: 40px;
-  right: 20px;
-  bottom: 20px;
-  z-index: 1;
+  right: 15px;
+  bottom: 15px;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  z-index: +1;
+`;
+
+const Text = styled.div.attrs(props => ({
+  style: {
+    paddingRight: `${props.hovered * 125 + 5}px`,
+    opacity: props.hovered ? 1.0 : 0,
+  },
+}))`
+  position: absolute;
+  display: inline-block;
+  right: 1px;
+  padding-left: 10px;
+  width: 50px;
+  bottom: 0;
+  font-size: 16px;
+  line-height: 50px;
+  border-radius: 25px;
+  background: ${COLORS.gray[200]};
+  z-index: -1;
+  transform-origin: center right;
+  overflow: hidden;
+  cursor: pointer;
+  user-select: none; /* diable text-selection */
+`;
+
+const Icon = styled(IconBase).attrs(props => ({
+  size: props.size,
+  style: {
+    transform: `scale(${props.hovered / 5 + 1}, ${props.hovered / 5 + 1})`,
+  },
+}))`
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  background: ${COLORS.white};
+  color: ${props => (!!props.hovered ? props.hovercolor : props.color)};
+  border-radius: 50% 50%;
+  border: 2px solid ${COLORS.gray[200]};
+  cursor: pointer;
+  & > svg {
+    transform-origin: center center;
+    transform: translateY(-2px); /* slightly move icon up */
+  }
 `;
 
 export default FeedbackButton;
