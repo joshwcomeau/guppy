8c61992f8babbd833cb8d46eb1261f58553c686c
AWolf81 <awolf2904@gmail.com>
2019-02-10 21:49:41 +0100

    added a constant instead of Date.now()

--- src/components/DependencyManagementPane/__mocks__/dependency.js
+++ src/components/DependencyManagementPane/__mocks__/dependency.js
@@ -3,6 +3,6 @@ export const mockReactHit = {
   name: 'React',
   version: '16.0.0',
   license: 'MIT',
-  modified: Date.now(),
+  modified: 1549828927372,
   humanDownloadsLast30Days: 1000000,
 };
