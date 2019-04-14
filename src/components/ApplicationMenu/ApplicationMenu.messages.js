21f14740059c1bd87c8cdfc645893a569898a511
Zian Ke <kezian@umich.edu>
2019-04-13 20:54:19 -0400

    Init message descriptor for ApplicationMenu

---
+++ src/components/ApplicationMenu/ApplicationMenu.messages.js
@@ -0,0 +1,16 @@
+// @flow
+import { defineMessages } from 'react-intl';
+
+const messages = {
+  languageCaptions: {
+    en: {
+      id: 'applicationMenu.languageCaptions.en',
+      defaultMessage: 'English',
+      description: 'Language caption for "en"',
+    },
+  },
+};
+
+const definedMessages = defineMessages(messages);
+
+export default definedMessages;
