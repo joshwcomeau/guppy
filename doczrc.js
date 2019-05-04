3ec75805828fa0951861c1394c9c061559bce239
Melanie Seltzer <melleh11@gmail.com>
2019-05-04 13:19:00 -0700

    Address PR comments

--- doczrc.js
+++ doczrc.js
@@ -11,7 +11,19 @@ export default {
     colors: {
       primary: RAW_COLORS.blue[700],
     },
+    styles: {
+      // docz-theme-default global body styles (specifically, line-height) is interfering with
+      // xsmall btn component. This is a hacky override so btns get a line-height of 1.
+      body: `
+        font-family: "Source Sans Pro", sans-serif;
+        font-size: 16px;
+        line-height: 1;
+        p, a, h1, h2, h3, h4, table {
+          line-height: 1.6;
+        }
+      `,
+    },
   },
   plugins: [css()],
-  menu: ['Guppy Style Guide', 'Components', 'Reusable Components', 'Shared'],
+  menu: ['Guppy Style Guide', 'Components', 'Reusable Components', 'Colors'],
 };
