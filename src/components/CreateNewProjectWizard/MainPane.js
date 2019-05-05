58c88494198b1c80d706e992e44b425813f2b8b9
AWolf81 <AWolf2904@gmail.com>
2019-05-05 23:58:12 +0200

    add interpolation to fix ProjectName offset issue

--- src/components/CreateNewProjectWizard/MainPane.js
+++ src/components/CreateNewProjectWizard/MainPane.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { PureComponent, Fragment } from 'react';
-import { Spring, animated } from 'react-spring';
+import { Spring, animated, interpolate } from 'react-spring';
 import styled from 'styled-components';
 
 import FormField from '../FormField';
@@ -146,6 +146,7 @@ class MainPane extends PureComponent<Props> {
     } = this.props;
 
     const { lastIndex, steps } = this.renderConditionalSteps(currentStepIndex);
+    console.log(currentStepIndex);
     return (
       <Fragment>
         <Spring
@@ -191,9 +192,9 @@ class MainPane extends PureComponent<Props> {
   }
 }
 
-const Wrapper = animated(styled.div.attrs(props => ({
+const Wrapper = animated(styled.div.attrs(({ translateY }) => ({
   style: {
-    transform: `translateY(${props.translateY}px)`,
+    transform: interpolate([translateY], y => `translateY(${y}px)`),
   },
 }))`
   height: 75vh;
