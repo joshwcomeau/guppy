2fc65a65e3e7e6647a9ad17642ea5eae9f064c24
Melanie Seltzer <melleh11@gmail.com>
2019-05-08 23:05:17 -0700

    Convert lingering storybook components, create docz specific component folder

---
+++ src/docz/components/ProgressManager/ProgressManager.js
@@ -0,0 +1,40 @@
+// @flow
+import React, { Fragment, Component } from 'react';
+import { StrokeButton } from '../../../components/Button';
+
+type Props = { children: (data: any) => React$Node };
+type State = { progress: number };
+
+class ProgressManager extends Component<Props, State> {
+  state = {
+    progress: 0.5,
+  };
+
+  updateProgress = () => {
+    const { progress } = this.state;
+
+    const nextProgress = progress === 1 ? 0 : progress + 0.25;
+
+    this.setState({ progress: nextProgress });
+  };
+
+  render() {
+    const { children } = this.props;
+    const { progress } = this.state;
+
+    return (
+      <Fragment>
+        {children({
+          progress,
+          updateProgress: this.updateProgress,
+        })}
+        <br />
+        <StrokeButton size="small" onClick={this.updateProgress}>
+          Generate new value
+        </StrokeButton>
+      </Fragment>
+    );
+  }
+}
+
+export default ProgressManager;
